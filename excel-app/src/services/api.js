d6324a2835220cfd1ee1df85e2c04af92d16dd55
Luis Alberto Ibarra Calderon <ibarra.luisalberto23@gmail.com>
2026-04-22 15:33:15 -0400

    nuevo

--- excel-app/src/services/api.js
+++ excel-app/src/services/api.js
@@ -1,7 +1,7 @@
-const BASE_URL = "http://127.0.0.1:8000";
+//const BASE_URL = "http://127.0.0.1:8000";
 
 //Base url de Render
-//const BASE_URL = "https://api-admin-shc170.onrender.com";
+const BASE_URL = "https://api-admin-shc170.onrender.com";
 
 export const api = {
 
