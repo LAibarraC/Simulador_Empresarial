3c584685472570d3a2d49088431b92a93876c5e7
Luis Alberto Ibarra Calderon <ibarra.luisalberto23@gmail.com>
2026-07-10 08:51:18 -0400

    backend

--- Backend/database.py
+++ Backend/database.py
@@ -16,10 +16,10 @@ DB_NAME = os.getenv("DB_NAME")
 
 # En producción, leemos la URL de la base de datos desde variables de entorno.
 # Si no está definida, usamos la conexión por defecto de XAMPP local.
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") or "mysql+pymysql://root:23luis99@localhost:3306/estadistica_db"
+#SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") or "mysql+pymysql://root:23luis99@localhost:3306/estadistica_db"
 
 
-#SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
 # Si la URL empieza con mysql:// (sin especificar driver), forzamos mysql+pymysql:// para SQLAlchemy
 if SQLALCHEMY_DATABASE_URL.startswith("mysql://"):
